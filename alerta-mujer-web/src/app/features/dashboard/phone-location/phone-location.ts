8377b09545139463af35774e0cf0ac8d79218100
JoseSebastianSuarezCumaco <masquebugs1@gmail.com>
2026-06-10 16:43:00 -0500

    feat: agregar modulos phone-location

---
+++ alerta-mujer-web/src/app/features/dashboard/phone-location/phone-location.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-phone-location',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './phone-location.html',
+  styleUrl: './phone-location.scss'
+})
+export class PhoneLocationComponent {}

249875f26fecf65166473bc80aedeb1038974a90
JoseSebastianSuarezCumaco <masquebugs1@gmail.com>
2026-06-11 13:00:05 -0500

    feature: agregar location history, layout y navegacion del dashboard

--- alerta-mujer-web/src/app/app.routes.ts
+++ alerta-mujer-web/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
 import { RegisterComponent } from './features/auth/register/register';
+import { DashboardLayoutComponent } from './core/layouts/dashboard-layout/dashboard-layout';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
@@ -15,6 +16,7 @@ export const routes: Routes = [
 
   {
     path: 'dashboard',
+    component: DashboardLayoutComponent,
     children: [
       {
         path: '',
